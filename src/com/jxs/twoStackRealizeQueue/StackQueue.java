5acacbe17598c915b0c05f3c391aaaff689d76ca
Jiangxs <songxjxs@yeah.ent>
2018-07-03 18:11:44 +0800

    两个栈实现队列

---
+++ src/com/jxs/twoStackRealizeQueue/StackQueue.java
@@ -0,0 +1,40 @@
+package com.jxs.twoStackRealizeQueue;
+
+import org.junit.Test;
+
+import java.util.Stack;
+
+public class StackQueue {
+
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+
+        stack1.push(node);
+    }
+
+    public int pop() throws Exception {
+
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        if (stack2.isEmpty()) {
+            throw new Exception();
+        }
+        return stack2.pop();
+    }
+
+    @Test
+    public void test() throws Exception {
+
+        push(1);
+        push(2);
+        push(3);
+        System.out.println(pop());
+        System.out.println(pop());
+        System.out.println(pop());
+    }
+}
