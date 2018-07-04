ea4e2ee33c1b9e552165501a48daaca4203866c4
Jiangxs <songxjxs@yeah.ent>
2018-07-04 09:14:51 +0800

    调整数组顺序，使奇数在前，偶数在后

---
+++ src/com/jxs/reOrderArray/ReOrderArray.java
@@ -0,0 +1,36 @@
+package com.jxs.reOrderArray;
+
+import org.junit.Test;
+
+public class ReOrderArray {
+
+    public void reOrderArray(int [] array) {
+
+        int oddNumCount = 0;
+        for (int num : array) {
+            if (num % 2 != 0) {
+                oddNumCount++;
+            }
+        }
+        int i = 0;
+        int j = oddNumCount;
+        int[] copyArray = array.clone();
+        for (int num : copyArray) {
+            if (num % 2 != 0) {
+                array[i++] = num;
+            } else {
+                array[j++] = num;
+            }
+        }
+    }
+
+    @Test
+    public void test() {
+
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+        reOrderArray(array);
+        for (int num : array) {
+            System.out.print(num+" ");
+        }
+    }
+}
