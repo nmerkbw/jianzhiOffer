b5b6f94f94febc52db7bbaa021370fb419b3858e
Jiangxs <songxjxs@yeah.ent>
2018-07-04 09:11:47 +0800

    斐波拉契的计算

---
+++ src/com/jxs/fibonacci/Fibonacci.java
@@ -0,0 +1,29 @@
+package com.jxs.fibonacci;
+
+import org.junit.Test;
+
+public class Fibonacci {
+
+    public int fibonacci(int n) {
+
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+
+        int sum = 0;
+        int s1 = 1;
+        int s2 = 1;
+        for (int i = 3; i <= n; i++) {
+            sum = s1 + s2;
+            s1 = s2;
+            s2 = sum;
+        }
+        return sum;
+    }
+
+    @Test
+    public void test() {
+
+        System.out.println(fibonacci(9));
+    }
+}
