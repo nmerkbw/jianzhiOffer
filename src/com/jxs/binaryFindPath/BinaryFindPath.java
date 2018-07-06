c4428fe843269c285bb03e1c4fd53863ee0555fb
Jiangxs <songxjxs@yeah.ent>
2018-07-06 19:36:50 +0800

    根据整数值搜索二叉树的路径

--- src/com/jxs/binaryFindPath/BinaryFindPath.java
+++ src/com/jxs/binaryFindPath/BinaryFindPath.java
@@ -1,5 +1,7 @@
 package com.jxs.binaryFindPath;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 
 /**
@@ -20,24 +22,41 @@ public class BinaryFindPath {
 
     private ArrayList<ArrayList<Integer>> list = new ArrayList<>();
 
-    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
+    public ArrayList<ArrayList<Integer>> findPath(TreeNode root, int target) {
 
+        ArrayList<Integer> path = new ArrayList<>();
+        backTracking(root, target, path);
         return list;
     }
 
-    public void backTracking(TreeNode root, int target, ArrayList<Integer> path) {
+    private void backTracking(TreeNode root, int target, ArrayList<Integer> path) {
 
         if (root == null) {
             return;
         }
         path.add(root.val);
         target = target - root.val;
+        // 路径上的值的和相等，且已经到达叶子
         if (target == 0 && root.left == null && root.right == null) {
+            // 将路径添加到集合中
             list.add(new ArrayList<>(path));
         } else {
             backTracking(root.left, target, path);
             backTracking(root.right, target, path);
         }
+        // 到达此处说明已经走到节点的终点，将路径中上一次的节点删除，回溯到之前的结点上
         path.remove(path.size() - 1);
+    }
+
+    @Test
+    public void test() {
+
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(12);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(7);
+
+        System.out.println(findPath(root,22));
     }
 }
