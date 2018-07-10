c890405bdea875bcbd5745a6e248f45a7c4600c4
Jiangxs <songxjxs@yeah.ent>
2018-07-10 13:58:13 +0800

    回溯问题：字符串的全排列

---
+++ src/com/jxs/permutation/Permutation.java
@@ -0,0 +1,51 @@
+package com.jxs.permutation;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Permutation {
+
+    private ArrayList<String> ret = new ArrayList<>();
+    public ArrayList<String> permutation(String str) {
+
+        if (str.length() == 0) {
+            return ret;
+        }
+        char[] chars = str.toCharArray();
+        Arrays.sort(chars);
+        backTracking(chars,new boolean[chars.length],new StringBuilder());
+        return ret;
+    }
+
+    private void backTracking(char[] chars, boolean[] hasUsed, StringBuilder s) {
+
+        if (s.length() == chars.length) {
+            ret.add(s.toString());
+            return;
+        }
+
+        for (int i = 0; i < chars.length; i++) {
+            if (hasUsed[i]) {
+                continue;
+            }
+            if (i != 0 && chars[i] == chars[i - 1] && !hasUsed[i - 1]) {
+                continue;
+            }
+            hasUsed[i] = true;
+            s.append(chars[i]);
+            backTracking(chars, hasUsed, s);
+            s.deleteCharAt(s.length() - 1);
+            hasUsed[i] = false;
+        }
+    }
+
+    @Test
+    public void test() {
+
+        String str = "";
+        ArrayList<String> list = permutation(str);
+        System.out.println(list);
+    }
+}
