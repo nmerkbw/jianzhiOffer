35a04c9ae23ccffc99f04242325cca61abfafe4a
Jiangxs <songxjxs@yeah.ent>
2018-07-16 20:17:50 +0800

    正则表达式匹配（背包问题）

---
+++ src/com/jxs/_52_match/Match.java
@@ -0,0 +1,46 @@
+package com.jxs._52_match;
+
+import org.junit.Test;
+
+/**
+ * 太难了
+ * 动态规划问题+背包问题
+ * 理解障碍：dp数组
+ * */
+public class Match {
+
+    public boolean match(char[] str, char[] pattern) {
+
+        int m = str.length, n = pattern.length;
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= n; i++) {
+            if (pattern[i - 1] == '*') {
+                dp[0][i] = dp[0][i - 2];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str[i - 1] == pattern[j - 1] || pattern[j - 1] == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pattern[j - 1] == '*') {
+                    if (pattern[j - 2] == str[i - 1] || pattern[j - 2] == '.') {
+                        dp[i][j] = dp[i][j - 1] || dp[i][j - 2] || dp[i - 1][j];
+                    } else {
+                        dp[i][j] = dp[i][j - 2];
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    @Test
+    public void test() {
+
+        char[] str = {'a', 'a', 'a'};
+        char[] parttern = {'a', '*', 'a'};
+        match(str, parttern);
+    }
+}
