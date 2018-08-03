eaff05b47d330aacb79f61fb020bdc8ce1445501
Jiangxs <songxjxs@yeah.ent>
2018-08-04 07:54:24 +0800

    合并两个单调增的链表，合并完成之后链表单调增

--- src/com/jxs/listMerge/ListNodeMerge.java
+++ src/com/jxs/listMerge/ListNodeMerge.java
@@ -16,7 +16,7 @@ public class ListNodeMerge {
         }
     }
 
-    public ListNode merge(ListNode list1, ListNode list2) {
+    public ListNode mergeSolution1(ListNode list1, ListNode list2) {
 
         ListNode head = new ListNode(-1);
         ListNode newNode = head;
@@ -40,6 +40,41 @@ public class ListNodeMerge {
         return head.next;
     }
 
+    /**
+     * 自己写的方法，好懂
+     * */
+    public ListNode mergeSolution2(ListNode list1,ListNode list2) {
+        if (list1 == null && list2 == null) {
+            return null;
+        }
+        if (list1 == null && list2 != null) {
+            return list2;
+        }
+        if (list2 == null && list1 != null) {
+            return list1;
+        }
+        ListNode newNode = new ListNode(0);
+        ListNode ret = newNode;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                newNode.next = new ListNode(list1.val);
+                newNode = newNode.next;
+                list1 = list1.next;
+            } else {
+                newNode.next = new ListNode(list2.val);
+                newNode = newNode.next;
+                list2 = list2.next;
+            }
+        }
+
+        if (list1 != null && list2 == null) {
+            newNode.next = list1;
+        } else if (list2 != null && list1 == null) {
+            newNode.next = list2;
+        }
+        return ret.next;
+    }
+
     @Test
     public void test() {
 
@@ -50,7 +85,7 @@ public class ListNodeMerge {
         node2.next = new ListNode(4);
         node2.next.next = new ListNode(6);
 
-        ListNode node = merge(node1, node2);
+        ListNode node = mergeSolution2(node1, node2);
         while (node != null) {
             System.out.println(node.val);
             node = node.next;
